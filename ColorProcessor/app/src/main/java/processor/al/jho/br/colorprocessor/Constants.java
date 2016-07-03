8864429b942d75cd0cd35b30d61e2388657b9554
JhoanesFreitas <jhoanesbarbosa@gmail.com>
2016-07-03 16:01:09 -0300

    constants e obtendo instância de algumas variáveis

---
+++ ColorProcessor/app/src/main/java/processor/al/jho/br/colorprocessor/Constants.java
@@ -0,0 +1,15 @@
+package processor.al.jho.br.colorprocessor;
+
+/**
+ * Created by jhoanesfreitas on 03/07/16.
+ */
+public class Constants{
+
+  public static final String BLUE = "A";
+  public static final String GREEN = "VD";
+  public static final String RED = "VM";
+  public static final String GREY = "C";
+  public static final String BLACK = "P";
+  public static final String PURPLE = "R";
+
+}
